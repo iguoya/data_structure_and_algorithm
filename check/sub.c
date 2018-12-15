27ec2350faa4609d77c54ebc17c7c7a3a8c3afc8
tiger <375478250@qq.com>
2018-12-15 18:17:28 +0800

    将基础练习内容放到basic分支

--- check/sub.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by tiger on 18-10-24.
-//
-
-#include "sub.h"
-
-int sub(int a, int b) {
-    return a-b;
-}
--- check/sub.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by tiger on 18-10-24.
-//
-
-#ifndef CHECK_SUB_H
-#define CHECK_SUB_H
-int sub(int a, int b);
-#endif //CHECK_SUB_H
